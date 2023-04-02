b2dcae59a3b9750484604d6b4b869622a2cb834d
FEguare <123463013+FEguare@users.noreply.github.com>
2023-04-02 22:57:41 +0100

    Lava area obstacle added as prefab

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "Racer") {
+            other.gameObject.GetComponent<SpriteRenderer>().sprite = other.gameObject.GetComponent<Controller>().CarSprites[1];
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.name == "Racer") {
+            other.gameObject.GetComponent<Controller>().velocity *= 0.85f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        other.gameObject.GetComponent<Controller>().keepBurning = true;
+    }
+}
