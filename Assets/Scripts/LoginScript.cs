fc936f58b3cefecd49d6c61b7c9639aaea82df43
Nollaig McHugh <mchughno@tcd.ie>
2023-04-05 12:58:41 +0100

    Added input fields for email and password

--- Assets/Scripts/LoginScript.cs
+++ Assets/Scripts/LoginScript.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+using TMPro;
 
 public class LoginScript : MonoBehaviour
 {
-    public void BeginLogIn() { 
-        //todo add log in functionality
+    public TMP_InputField input_email;
+    public TMP_InputField input_password;
 
+    public void BeginLogIn() { 
+        //todo add log in functionality 
+
+    }
+
+    public void onEndEditEmail() {
+        // todo log email
+        Debug.Log("EMAIL " + input_email.text);
+    } 
+
+    public void onRegisterSelect() {
+        // todo
+        Debug.Log("REGISTER");
+    }
+
+    public void onEndEditPassword() {
+        // todo log email
+        Debug.Log("PASSWORD " + input_password.text); 
     }
 }
