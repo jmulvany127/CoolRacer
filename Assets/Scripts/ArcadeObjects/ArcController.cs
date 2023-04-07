96069099d229a69084de7aa45e92f2ab6a8da8e5
CormacS-TCD <csharkey@tcd.ie>
2023-04-07 21:59:37 +0100

    Mapped sprites to objects

--- Assets/Scripts/ArcadeObjects/ArcController.cs
+++ Assets/Scripts/ArcadeObjects/ArcController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArcadeController : MonoBehaviour
+public class ArcController : MonoBehaviour
 {
     
     public FixedJoystick Joystick;
