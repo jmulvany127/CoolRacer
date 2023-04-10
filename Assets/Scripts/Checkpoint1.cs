183d30c95e294d31f779418bc5fbdadbf859f460
jmulvany <jmulvany@tcd.ie>
2023-04-10 06:05:07 +0100

    added sprites and finished level 1

---
+++ Assets/Scripts/Checkpoint1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint1 : MonoBehaviour
+{  
+    //Respawn point is the origin if no checkpoints have been crossed
+    private Vector3 respawnPoint = new Vector2(0,0);
+    
+    //if triggered by the racer change the respawn point to the current position 
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "Racer") {
+            
+            respawnPoint = transform.position;
+        }
+    }
+    //this fucntion will return the respawn point when called
+    public Vector3 GetRespawn(){
+        return respawnPoint;
+    }
+
+}
--- Assets/Scripts/portal3.cs
+++ Assets/Scripts/portal3.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class portal : MonoBehaviour
+public class portal3 : MonoBehaviour
 {
     
-    private Vector3 newRespawnPoint;
+    public Vector3 newRespawnPoint;
     //wehn triggered by the racer change its position to the latestest respawn point and reset its velocity and angle 
     private void OnTriggerEnter2D(Collider2D other) {
-        newRespawnPoint =  FindObjectOfType<Checkpoint>().GetRespawn();
+        newRespawnPoint =  FindObjectOfType<Checkpoint3>().GetRespawn();
         if (other.gameObject.name == "Racer") {
             other.transform.position = newRespawnPoint;
             FindObjectOfType<Controller>().ResetPosition();
