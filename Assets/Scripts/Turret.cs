96be522cb9534bfbc9274f84e9ca8312d65e7ea4
jmulvany <jmulvany@tcd.ie>
2023-04-13 07:08:46 +0100

    Built level 3, fixed turrets to shoot through vollision boxes, fixed level 2 portals

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -42,6 +42,10 @@ public class Turret : MonoBehaviour
                 }
             }
         }
+        else
+        {
+            Detected = false; // Target is out of range, stop firing.   
+        }
 
         if (Detected)
         {
